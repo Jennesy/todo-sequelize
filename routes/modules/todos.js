26e9ff1adacd3d808b97033551da0adb94f4eef4
Jennesy <jennesy.dai@gmail.com>
2021-06-15 01:43:04 +0800

    feat: add new, edit, delete functions

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,6 +4,18 @@ const db = require('../../models')
 const User = db.User
 const Todo = db.Todo
 
+router.get('/new', (req, res) => {
+  res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const UserId = req.user.id
+  const name = req.body.name
+  return Todo.create({ name, UserId })
+    .then(() => { res.redirect('/') })
+    .catch(error => console.log(error))
+})
+
 router.get('/:id', (req, res) => {
   const id = req.params.id
   return Todo.findByPk(id)
@@ -13,4 +25,32 @@ router.get('/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then((todo) => res.render('edit', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+router.put('/:id', (req, res) => {
+  const { name, isDone } = req.body
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => todo.destroy())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 module.exports = router
