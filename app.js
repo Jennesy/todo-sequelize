8128a2c4ebd7e8fefcd06297579f44141097ced2
Jennesy <jennesy.dai@gmail.com>
2021-06-14 16:33:45 +0800

    feat: add local login function

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
+const passport = require('passport')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
-
+const usePassport = require('./config/passport')
 const db = require('./models')
 const User = db.User
 const Todo = db.Todo
@@ -12,8 +13,17 @@ const Todo = db.Todo
 const app = express()
 const port = 3000
 
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+usePassport(app)
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
@@ -38,9 +48,10 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
